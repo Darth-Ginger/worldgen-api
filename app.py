c3bd98d76fd75de7cd02970257229a68efe80554
Darth-Ginger <Justin.a.schwarz@gmail.com>
2024-05-18 14:14:33 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Worlds/Arinthia/04_factions.json
    	modified:   Worlds/Arinthia/05_kingdoms.json
    	new file:   Worlds/Arinthia/06_key_leaders.json
    	renamed:    Worlds/Arinthia/06_history_chunks.json -> Worlds/Arinthia/07_history_chunks.json
    	modified:   app.py
    	new file:   app.py.old
    	modified:   static/css/style.css
    	new file:   templates/base.html
    	new file:   templates/factions.html
    	new file:   templates/index.html
    	new file:   templates/kingdoms.html
    	new file:   templates/leaders.html
    	modified:   templates/sidebar.html
    	new file:   templates/world.html

--- app.py
+++ app.py
@@ -1,41 +1,63 @@
-import os
+from flask import Flask, render_template, jsonify, url_for
 import json
-from flask import Flask, jsonify, render_template
+import os
+
 
 app = Flask(__name__)
-data_dir = "./Worlds"  # Replace with the path to your data directory
+
+worlds_dir = 'Worlds'
+
 
 @app.context_processor
 def inject_sidebar():
     """Injects the directory contents into the template context."""
-    sidebar_items = os.listdir(data_dir)
-    return {"sidebar_items": sidebar_items}
+    
+    worlds = []
+    
+    for root, dirs, files in os.walk(worlds_dir):
+        for dir_name in dirs:
+            world = {
+                'name': dir_name,
+                'url': url_for('world', world_name=dir_name)
+            }
+            worlds.append(world)
+        break
+    
+    return dict(worlds=worlds)
+
+def load_json(filepath):
+    with open(filepath, 'r') as file:
+        return json.load(file)
 
 @app.route('/')
 def index():
-    worlds = os.listdir(data_dir)
-    
-    return render_template('worlds_page.html', worlds=worlds) 
+    return render_template('index.html')
 
-@app.route('/<world>')
-def get_world(world):
-    world_dir = os.path.join(data_dir, world)
-    world_data = os.listdir(world_dir)
-    if os.path.isdir(world_dir):
-        return render_template('world_page.html', world=world, world_data=world_data)
-    return 
+@app.route('/world/<world_name>')
+def world(world_name):
+    world_dir = os.path.join('Worlds', world_name)
+    if not os.path.exists(world_dir):
+        return "World not found", 404
+    description = load_json(os.path.join(world_dir, '01_world_description.json'))
+    return render_template('world.html', world_name=world_name, description=description)
 
-@app.route('/<world>/<filename>')
-def get_data(world, filename):
-    if ".json" not in filename:
-        filename = f"{filename}.json"
-    file_path = os.path.join(data_dir, world, f"{filename}")
-    if os.path.exists(file_path):
-        with open(file_path) as f:
-            data = json.load(f)
-        return jsonify(data)
-    else:
-        return "File not found", 404
+@app.route('/world/<world_name>/kingdoms')
+def kingdoms(world_name):
+    filepath = os.path.join('Worlds', world_name, 'kingdoms.json')
+    data = load_json(filepath)
+    return render_template('kingdoms.html', kingdoms=data['kingdoms'], world_name=world_name)
+
+@app.route('/world/<world_name>/factions')
+def factions(world_name):
+    filepath = os.path.join('Worlds', world_name, 'factions.json')
+    data = load_json(filepath)
+    return render_template('factions.html', factions=data['factions'], world_name=world_name)
+
+@app.route('/world/<world_name>/leaders')
+def leaders(world_name):
+    filepath = os.path.join('Worlds', world_name, 'leaders.json')
+    data = load_json(filepath)
+    return render_template('leaders.html', leaders=data['leaders'], world_name=world_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True) 
